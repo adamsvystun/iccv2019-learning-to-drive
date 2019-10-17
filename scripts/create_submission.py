f515ac0e2fe34b118a95174dbc7c17fb8b696655
Adam Svystun <adam.svystun@gmail.com>
2019-10-17 19:44:03 -0400

    Update comments in the script

--- scripts/create_submission.py
+++ scripts/create_submission.py
@@ -16,6 +16,7 @@ def infer_frequency(
         original_dataset_descriptor: pd.DataFrame,
         sampled_dataset_descriptor: pd.DataFrame
 ) -> int:
+    """Infer the sampling frequency of the dataset, so we don't ask the user for redundant information"""
     original_chapter_one = original_dataset_descriptor[original_dataset_descriptor.chapter == 0]
     sampled_chapter_one = sampled_dataset_descriptor[sampled_dataset_descriptor.chapter == 0]
     return math.floor(float(len(original_chapter_one.index)) / len(sampled_chapter_one.index))
@@ -31,7 +32,9 @@ def interpolate(
 ) -> pd.DataFrame:
     interpolated_submissions = []
     sequence_length = frequency * number
+    # Calculate the initial offset of the first predicted frame under given configuration
     initial_offset = sample_frequency * sequence_length
+    # For each chapter
     for chapter in original_dataset_descriptor.chapter.unique():
         # Get chapters
         original_chapter = original_dataset_descriptor[original_dataset_descriptor.chapter == chapter]
@@ -45,11 +48,14 @@ def interpolate(
         old_indices = list(range(initial_offset, (len(submission_cut.index) + sequence_length) * sample_frequency, sample_frequency))
         submission_cut = submission_cut.set_index(pd.Index(old_indices))
         new_indices = range(0, original_chapter_len)
+        # Start from 100s frame in the test data
         new_indices = new_indices[100:original_chapter_len]
         submission_cut = submission_cut.reindex(new_indices)
+        # Interpolate (replace NaN's) in the data frame in both directions
         submission_cut = submission_cut.interpolate(limit_direction='both')
         # Append new submission to a list
         interpolated_submissions.append(submission_cut)
+    # Return concatenated data frame
     return pd.concat(interpolated_submissions, axis=0)
 
 
@@ -57,7 +63,7 @@ def save_submission(df: pd.DataFrame, filepath: str) -> None:
     df.to_csv(filepath, index=False)
 
 
-def interpolate_submission(options) -> None:
+def create_submission(options) -> None:
     original_dataset_descriptor = read_descriptor(options.original_dataset_path)
     sampled_dataset_descriptor = read_descriptor(options.sampled_dataset_path)
     submission = read_submission(options.input_path)
@@ -100,4 +106,4 @@ if __name__ == '__main__':
     )
 
     args = parser.parse_args()
-    interpolate_submission(args)
+    create_submission(args)
